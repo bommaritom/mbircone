8fbd2b44327fb9e9e5f0eb71544c1ec4d7fd79d1
bommaritom <bommaritom@carleton.edu>
2023-01-11 12:24:00 -0500

    write working test scripts for small-scale and for shepp logan

--- demo/test_block_average_shepp_logan.py
+++ demo/test_block_average_shepp_logan.py
@@ -41,8 +41,8 @@ print('\t The goal of this script is to test block average (say more)\
 # Change the parameters below for your own use case.
 
 # Detector size
-num_det_rows = 13
-num_det_channels = 13
+num_det_rows = 128
+num_det_channels = 128
 # Geometry parameters
 magnification = 1.0  # Ratio of (source to detector)/(source to center of rotation)
 dist_factor = 1.0
@@ -52,31 +52,35 @@ num_views = 64
 # projection angles will be uniformly spaced within the range [0, 2*pi).
 angles = np.linspace(0, 2 * np.pi, num_views, endpoint=False)
 # qGGMRF recon parameters
-sharpness = 0.1  # Controls regularization: larger => sharper; smaller => smoother
+sharpness = 0.0  # Controls regularization: larger => sharper; smaller => smoother
 T = 0.1  # Controls edginess of reconstruction
 # display parameters
 vmin = 0.10
 vmax = 0.12
 
 # Size of phantom
-num_slices_phantom = 5
-num_rows_phantom = 5
-num_cols_phantom = 5
+num_phantom_rows = 128
+num_phantom_cols = 128
+num_phantom_slices = 128
+scale = 1.0
 delta_pixel_phantom = 1
+
+# Calculate scaling factor for Shepp Logan phantom so that projections are physically realistic -log attenuation values
+SL_phantom_density_scale = 4.0*magnification/(scale*num_phantom_rows)
+
+
 
 # local path to save phantom, sinogram, and reconstruction images
 save_path = f'output/test_block_average/'
 os.makedirs(save_path, exist_ok=True)
 
-print('Genrating phantom ...')
+print('Genrating 3D Shepp Logan phantom ...\n')
 ######################################################################################
-# Generate phantom
+# Generate a 3D shepp logan phantom
 ######################################################################################
-
-phantom = np.zeros((num_slices_phantom, num_rows_phantom, num_cols_phantom))
-phantom[num_slices_phantom//2, num_rows_phantom//2, num_cols_phantom//2] = 2
-phantom[num_slices_phantom//2+1, num_rows_phantom//2+1, num_cols_phantom//2] = 0
-phantom[num_slices_phantom//2+2, num_rows_phantom//2+2, num_cols_phantom//2] = 1
+phantom = mbircone.phantom.gen_shepp_logan_3d(num_phantom_rows, num_phantom_cols, num_phantom_slices)
+phantom = SL_phantom_density_scale*phantom
+print('Phantom shape = ', np.shape(phantom))
 
 ######################################################################################
 # Generate synthetic sinograms
@@ -102,11 +106,11 @@ print('Synthetic sinogram shape: (num_views, num_det_rows, num_det_channels) = '
 ######################################################################################
 
 print('Performing 3D qGGMRF reconstruction ...\n')
-recon = mbircone.cone3D.recon(sino, angles, dist_source_detector, magnification,
-                              sharpness=sharpness, T=T, max_resolutions=0)
+# recon = mbircone.cone3D.recon(sino, angles, dist_source_detector, magnification,
+#                               sharpness=sharpness, T=T, max_resolutions=0)
 
 recon_low_res = mbircone.cone3D.recon(sino_block_average, angles, dist_source_detector, magnification,
-                                      det_channel_offset=-0.25, det_row_offset=-0.25,
+                                      det_channel_offset=0, det_row_offset=0,
                                       sharpness=sharpness, T=T, max_resolutions=0)
 
 ######################################################################################
@@ -139,20 +143,20 @@ for view_idx in [0, num_views // 4, num_views // 2]:
 #            filename=os.path.join(save_path, 'phantom_sagittal.png'), vmin=vmin, vmax=vmax)
 
 
-# Print recon images
-num_slices_recon, num_rows_recon, num_cols_recon = np.shape(recon)
-
-display_slice_recon = num_slices_recon // 2
-display_x_recon = num_rows_recon // 2
-display_y_recon = num_cols_recon // 2
-
-# recon images
-plot_image(recon[display_slice_recon], title=f'recon, axial slice {display_slice_recon}',
-           filename=os.path.join(save_path, 'recon_axial.png'), vmin=vmin, vmax=vmax)
-plot_image(recon[:, display_x_recon, :], title=f'recon, coronal slice {display_x_recon}',
-           filename=os.path.join(save_path, 'recon_coronal.png'), vmin=vmin, vmax=vmax)
-plot_image(recon[:, :, display_y_recon], title=f'recon, sagittal slice {display_y_recon}',
-           filename=os.path.join(save_path, 'recon_sagittal.png'), vmin=vmin, vmax=vmax)
+# # Print recon images
+# num_slices_recon, num_rows_recon, num_cols_recon = np.shape(recon)
+#
+# display_slice_recon = num_slices_recon // 2
+# display_x_recon = num_rows_recon // 2
+# display_y_recon = num_cols_recon // 2
+#
+# # recon images
+# plot_image(recon[display_slice_recon], title=f'recon, axial slice {display_slice_recon}',
+#            filename=os.path.join(save_path, 'recon_axial.png'), vmin=vmin, vmax=vmax)
+# plot_image(recon[:, display_x_recon, :], title=f'recon, coronal slice {display_x_recon}',
+#            filename=os.path.join(save_path, 'recon_coronal.png'), vmin=vmin, vmax=vmax)
+# plot_image(recon[:, :, display_y_recon], title=f'recon, sagittal slice {display_y_recon}',
+#            filename=os.path.join(save_path, 'recon_sagittal.png'), vmin=vmin, vmax=vmax)
 
 # Print recon low res images
 num_slices_recon_low_res, num_rows_recon_low_res, num_cols_recon_low_res = np.shape(recon_low_res)
@@ -163,11 +167,11 @@ display_y_recon_low_res = num_cols_recon_low_res // 2
 
 # recon low res images
 plot_image(recon_low_res[display_slice_recon_low_res], title=f'recon low res, axial slice {display_slice_recon_low_res}',
-           filename=os.path.join(save_path, 'recon_low_res_axial.png'), vmin=vmin, vmax=vmax)
+           filename=os.path.join(save_path, 'recon_low_res_axial.png'), vmin=vmin, vmax=.05)
 plot_image(recon_low_res[:, display_x_recon_low_res, :], title=f'recon low res, coronal slice {display_x_recon_low_res}',
-           filename=os.path.join(save_path, 'recon_low_res_coronal.png'), vmin=vmin, vmax=vmax)
+           filename=os.path.join(save_path, 'recon_low_res_coronal.png'), vmin=vmin, vmax=.05)
 plot_image(recon_low_res[:, :, display_y_recon_low_res], title=f'recon low res, sagittal slice {display_y_recon_low_res}',
-           filename=os.path.join(save_path, 'recon_low_res_sagittal.png'), vmin=vmin, vmax=vmax)
+           filename=os.path.join(save_path, 'recon_low_res_sagittal.png'), vmin=vmin, vmax=.05)
 
 print(f"Images saved to {save_path}.")
 input("Press Enter")
