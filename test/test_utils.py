69c8cedc1a435d329293c5d348fdac7bbcaa7ce5
bommaritom <bommaritom@carleton.edu>
2023-04-26 13:47:30 -0400

    add documentation for sigma in apply_gaussian_filter_to_sino

--- test/test_utils.py
+++ test/test_utils.py
@@ -116,6 +116,7 @@ def apply_gaussian_filter_to_sino(sino, sigma=1):
 
     Args:
         sino (float, ndarray): 3D sinogram data with shape (num_views, num_det_rows, num_det_channels).
+        sigma (float): Sigma value to be used in the Gaussian filter.
 
     Returns:
         (float, ndarray) 3D filtered sinogram data with shape (num_views, num_det_rows, num_det_channels).
